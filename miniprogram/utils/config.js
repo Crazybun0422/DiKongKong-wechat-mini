5638a13694f5fcf82bd485c89c37ddb4ac459d0e
MalcolmYo <Crazybun0422@163.com>
2025-10-30 20:33:46 +0800

    11111111

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,4 +1,6 @@
 const QQMAP_KEY = "GEDBZ-R36KT-S52XJ-LTI4K-WWZK7-USFNP"; // TODO: inject your Tencent Map key
+
+// const QQMAP_KEY = "QQXBZ-BRJKZ-AB4XD-7PUXM-KLXX6-5CFXZ";
 const DJI_PROXY = ""; // Optional: prefix to proxy DJI API requests
 const CAAC_TOKEN = "1e4b78fc-06bd-45be-8af7-cabd802ea9a8"; // CAAC WMS token
 
